7a98378b66e63be4ce4caf35274ee5285aa99690
Qmm <2268428576@qq.com>
2025-09-10 18:22:12 +0800

    rm clone due to sizde

--- src/platform_clients/mod.rs
+++ src/platform_clients/mod.rs
@@ -486,7 +486,7 @@ impl BuildV0Tx for temporal::Temporal {}
 impl BuildV0Tx for zeroslot::ZeroSlot {}
 impl BuildV0Tx for flash_block::FlashBlock {}
 
-pub trait Platform: Clone {
+pub trait Platform {
     /// 获取平台默认 TPS
     fn default_tps(&self) -> u64;
 }
